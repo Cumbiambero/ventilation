068dff71d0f58595b91b8003c895d31b10ca08f7
Cumbiambero <cumbiamberos@gmail.com>
2025-05-08 16:11:32 +0200

    Change pins to improve stripboard layout

--- ventilation.ino
+++ ventilation.ino
@@ -1,8 +1,8 @@
 #include <DHT.h> // source: https://github.com/adafruit/DHT-sensor-library
 
 #define PIN_DHT_SENSOR_READ 2  // pin for reading the temperature and humidity sensor
-#define PIN_DHT_SENSOR_POWER 6 // pin for powering the temperature and humidity sensor
-#define PIN_RELAY_FOR_FAN 7    // pin for the relay control that powers the 12v fan
+#define PIN_DHT_SENSOR_POWER 3 // pin for powering the temperature and humidity sensor
+#define PIN_RELAY_FOR_FAN 13   // pin for the relay control that powers the 12v fan
 
 DHT dht(PIN_DHT_SENSOR_READ, DHT11);
 float humidity = 0.0;
@@ -13,7 +13,7 @@ void setup() {
   pinMode(PIN_RELAY_FOR_FAN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(PIN_RELAY_FOR_FAN, LOW); 
-  digitalWrite(PIN_DHT_SENSOR_POWER, LOW); 
+  digitalWrite(PIN_DHT_SENSOR_POWER, LOW);
 }
 
 void loop() {  
